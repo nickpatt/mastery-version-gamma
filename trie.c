2782d365ce1d26b54ebbddbc5fcd487d385bfca7
Nickhil Pattikonda <reculas@cs.utexas.edu>
2025-04-19 18:58:58 -0500

    hi

--- trie.c
+++ trie.c
@@ -87,10 +87,8 @@ bool insert(trie_node_t* root, char* word) {
  * @param root the root of the Trie to clean up
  */
 void free_trie(trie_node_t* root) {
+    
     free_trie_node(&root);
-    for (int i = 0; i < ASCII_CHARS - FIRST_VALID_CHAR; i++) {
-        free_trie(root->children[i]);
-    }
 }
 
 static void free_trie_node(trie_node_t** node) {
